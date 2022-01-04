e1ab4161ed40102c5434e8eaf5640caa5917f9a8
Jason Qiu <jason_qiu@hotmail.com>
2022-01-04 12:45:33 +0800

    Created Sidebar.jsx

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -6,7 +6,7 @@ class Home extends React.Component {
     }
 
     render() {
-        return <h1>Hello world!!!!!!!!!!!</h1>;
+        return <h1>Hello world!</h1>;
     }
 }
 
---
+++ app/javascript/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Sidebar extends React.Component {
+    render() {
+        return <div></div>;
+    }
+}
+
+export default Sidebar;
