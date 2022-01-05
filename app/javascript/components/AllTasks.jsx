4eab346d2c2b2aab0fe8978df19ef319f3b6fa14
Jason Qiu <jason_qiu@hotmail.com>
2022-01-05 15:25:48 +0800

    Ignored .DS_Store

--- app/javascript/components/AllTasks.jsx
+++ app/javascript/components/AllTasks.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Task from "./Task";
 
 class AllTasks extends React.Component {
     render() {
-        return <h2>All tasks component</h2>;
+        return (
+            <div>
+                <h2>All Tasks</h2>
+                <Task />
+            </div>
+        );
     }
 }
 
--- app/javascript/components/TagTasks.jsx
+++ app/javascript/components/TagTasks.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Task from "./Task";
 
 class TagTasks extends React.Component {
     render() {
-        return <h2>Tag tasks component</h2>;
+        return (
+            <div>
+                <h2>Tag Tasks</h2>
+                <Task />
+            </div>
+        );
     }
 }
 
