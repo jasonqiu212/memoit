d28f8e82adbbf06ac4b015018953c765f6267a8d
Jason Qiu <jason_qiu@hotmail.com>
2022-01-24 17:23:05 +0800

    Edited application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "bootstrap";
+import "bootstrap-icons";
 import "./src/application.scss";
 import "../../assets/stylesheets/application.css";
 
