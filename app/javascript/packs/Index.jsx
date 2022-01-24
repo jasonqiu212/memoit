cf8510b6171d87fd8f3d83339aeefe1ce1606136
Jason Qiu <jason_qiu@hotmail.com>
2022-01-24 16:51:04 +0800

    Installed popperjs/core

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import $ from "jquery";
 import Popper from "popper.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "../../assets/stylesheets/style.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
