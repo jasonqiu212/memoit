78c814959fbf7de0bd7d1170da8eaa1830e02f8c
Jason Qiu <jason_qiu@hotmail.com>
2022-01-24 16:43:07 +0800

    Configured bootstrap to work for production

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,15 @@
 const { environment } = require("@rails/webpacker");
 const dotenv = require("dotenv");
+const webpack = require("webpack");
+
+environment.plugins.append(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    Popper: ["popper.js", "default"],
+  })
+);
 
 module.exports = environment;
 dotenv.config();
